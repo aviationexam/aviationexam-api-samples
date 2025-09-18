08594f1eab471a4f376b0ef168cff0a0b1e1f412
Ales Wojnar <ales@aviationexam.com>
2025-09-18 23:48:50 +0200

    Split contracts to separate files

---
+++ Aviationexam.Api.LmsSample/Models/GetLmsStudentExamOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aviationexam.LmsApiSample.Models;
+
+public class GetLmsStudentExamOutput
+{
+    public required int UserId { get; set; }
+
+    public required string SubjectName { get; set; }
+
+    public required long ExamId { get; set; }
+
+    public required string ExamName { get; set; }
+
+    public DateTime? DueBy { get; set; }
+
+    public required DateTime CompletedOn { get; set; }
+
+    public required decimal Score { get; set; }
+
+    public required int ElapsedTimeSeconds { get; set; }
+}
---
+++ Aviationexam.Api.LmsSample/Models/GetLmsStudentOutput.cs
@@ -0,0 +1,14 @@
+﻿namespace Aviationexam.LmsApiSample.Models;
+
+public class GetLmsStudentOutput
+{
+    public required int Id { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string Email { get; set; }
+
+    public string? LmsNote { get; set; }
+}
